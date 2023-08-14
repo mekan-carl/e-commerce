63bc3a45599aedbbd98aa45e75e2f290744a7d2c
Mekan Sahatvaliyev <smekan2320@gmail.com>
2023-08-14 20:04:11 +0300

    deploy

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -5,15 +5,15 @@ import SearchInput from "../components/SearchInput";
 
 export function Store() {
   return (
-    <Container>
+    <>
       <SearchInput />
-      <Row xs={1} md={2} lg={3} xl={4} className=" g-3">
+      <Row sm={1} md={2} lg={3} xl={4} className=" g-3">
         {storeitems.map((item) => (
           <Col key={item.id}>
             <StoreItem {...item} />
           </Col>
         ))}
       </Row>
-    </Container>
+    </>
   );
 }
